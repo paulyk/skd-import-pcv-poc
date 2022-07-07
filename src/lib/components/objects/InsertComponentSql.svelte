298dfb25caa21aa73f5d7e72b4d9853b841e3e4e
= <=>
2022-07-07 12:51:52 +0700

    feat: Add ImportComponents

---
+++ src/lib/components/objects/InsertComponentSql.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Card from "@components/elements/Card.svelte";
+  import type { CodeName } from "src/types";
+  export let components: CodeName[];
+
+  let sql = components.map(to_sql).join("\n");
+  function to_sql(input: CodeName) {
+    const r = `insert component (id, code, [name], dcwsRequired, createdAt) values( newid(),'${input.code}', '${input.name}' , 1,  getDate() );`;
+    return r;
+  }
+</script>
+
+<Card>
+  <div>Insert components SQL</div>
+  <pre>
+    {sql}
+</pre>
+</Card>
--- src/lib/components/objects/importComponents.svelte
+++ src/lib/components/objects/importComponents.svelte
@@ -4,6 +4,7 @@
   import Textarea from "@components/elements/Textarea.svelte";
   import { parseComponents } from "@lib/util/parseComponents";
   import type { CodeName } from "src/types";
+import InsertComponentSql from "./InsertComponentSql.svelte";
 
   let text = "";
   let components: CodeName[];
@@ -19,5 +20,8 @@
 {#if components.length > 0}
   <Row>
     <RefData title="Components" refData={components} />
+  </Row>
+  <Row>
+    <InsertComponentSql {components} />
   </Row>
 {/if}
