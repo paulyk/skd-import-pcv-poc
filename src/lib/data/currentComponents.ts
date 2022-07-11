a32f44f683dc015fea18b597df2165cd38d954a2
= <=>
2022-07-11 13:11:15 +0700

    feat: Add current components

---
+++ src/lib/data/currentComponents.ts
@@ -0,0 +1,126 @@
+export const currentComponents = [
+    {
+      "code": "AXR",
+      "name": "Axle rear"
+    },
+    {
+      "code": "B2",
+      "name": "Seat belt buckle row 2"
+    },
+    {
+      "code": "CUL",
+      "name": "Catalyst underbody left"
+    },
+    {
+      "code": "CUR",
+      "name": "Catalyst underbody right"
+    },
+    {
+      "code": "DA",
+      "name": "Driver airbag "
+    },
+    {
+      "code": "DKA",
+      "name": "Driver knee airbag "
+    },
+    {
+      "code": "DS",
+      "name": "Driver side seat airbag"
+    },
+    {
+      "code": "DSC",
+      "name": "Driver side curtain airbag"
+    },
+    {
+      "code": "EN",
+      "name": "Engine"
+    },
+    {
+      "code": "ENL",
+      "name": "Engine legal vo "
+    },
+    {
+      "code": "EXR",
+      "name": "Exhaust system rear"
+    },
+    {
+      "code": "FD",
+      "name": "Front differential unit"
+    },
+    {
+      "code": "FNL",
+      "name": "Frame number legal "
+    },
+    {
+      "code": "FT",
+      "name": "Fuel tank "
+    },
+    {
+      "code": "IK",
+      "name": "Ignition key"
+    },
+    {
+      "code": "PA",
+      "name": "Passenger airbag "
+    },
+    {
+      "code": "PAS",
+      "name": "Passenger seat"
+    },
+    {
+      "code": "PKA",
+      "name": "Passenger knee airbag "
+    },
+    {
+      "code": "PS",
+      "name": "Passenger side seat airbag"
+    },
+    {
+      "code": "PSC",
+      "name": "Passenger side curtain airbag"
+    },
+    {
+      "code": "RS",
+      "name": "Rear seat"
+    },
+    {
+      "code": "S1L",
+      "name": "Seatbelt row 1 left "
+    },
+    {
+      "code": "S1R",
+      "name": "Seatbelt row 1 right "
+    },
+    {
+      "code": "S2C",
+      "name": "Seatbelt row 2 center "
+    },
+    {
+      "code": "S2L",
+      "name": "Seatbelt row 2 left "
+    },
+    {
+      "code": "S2R",
+      "name": "Seatbelt row 2 right "
+    },
+    {
+      "code": "S3L",
+      "name": "Seatbelt row 3 left "
+    },
+    {
+      "code": "S3R",
+      "name": "Seatbelt row 3 right "
+    },
+    {
+      "code": "SCR",
+      "name": "Selective catalytic reduction"
+    },
+    {
+      "code": "TC",
+      "name": "Transfer case"
+    },
+    {
+      "code": "TR",
+      "name": "Transmission"
+    }
+  ]
